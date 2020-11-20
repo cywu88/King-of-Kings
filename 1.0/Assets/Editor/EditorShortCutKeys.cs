479eac413944810778bd87f60325232f4f85b4a7
easygamesniper <cywu88@126.com>
2020-11-20 20:04:13 +0800

    增加F5快捷启动方式

---
+++ 1.0/Assets/Editor/EditorShortCutKeys.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class EditorShortCutKeys : ScriptableObject
+{
+    [MenuItem("Edit/Launcher _F5")]
+    static void PlayGame()
+    {
+        if (!Application.isPlaying)
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/Launcher.unity", OpenSceneMode.Single);
+            //EditorSceneManager.SaveScene(SceneManager.GetActiveScene(), "", false);
+        }
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+    }
+}
