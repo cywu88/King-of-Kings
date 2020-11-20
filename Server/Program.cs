4c25093df7a4606dcefe01364edf8b5425870ee9
easygamesniper <cywu88@126.com>
2020-11-20 11:30:20 +0800

    提交一版简易服务器

---
+++ Server/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Server p = new Server();
+            while (true)
+            {
+                p.Update();
+            }
+        }
+    }
+}
