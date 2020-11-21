18a436863ff93f523cfc9d21778a0f06f179c79f
easygamesniper <cywu88@126.com>
2020-11-21 20:44:55 +0800

    更新资源加载方法

---
+++ 1.0/Assets/Scripts/Core/Components/ComponentExtension.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using XLua;
+
+[LuaCallCSharp]
+public static partial class ComponentExtension
+{
+    public static void AddParent(this Transform t, Transform parent, float size = 1f)
+    {
+        if (parent == null)
+        {
+            return;
+        }
+
+        t.SetParent(parent);
+        t.localPosition = Vector3.zero;
+        t.localScale = Vector3.one * size;
+        t.localEulerAngles = Vector3.zero;
+
+        RectTransform rt = t.GetComponent<RectTransform>();
+        if (rt != null)
+        {
+            rt.anchoredPosition = Vector2.zero;
+        }
+    }
+}
--- 1.0/Assets/Scripts/Core/ResourceMgr_New.cs
+++ 1.0/Assets/Scripts/Core/ResourceMgr_New.cs
@@ -1,8 +1,26 @@
-﻿
+﻿using UnityEngine;
+
 public class ResourceMgr_New : SingletonMonobehaviour<ResourceMgr_New>
 {
     public byte[] LoadLua(string luamodule)
     {
         return UabManager.LoadLua(luamodule);
-    } 
+    }
+
+    public GameObject CreateGameObjectFromResources(string respath, Transform parent)
+    {
+        respath = respath.Replace('.', '/');
+        var o = Resources.Load(respath);
+        if (o == null)
+        {
+            return null;
+        }
+        GameObject go = Instantiate(o) as GameObject;
+        if (parent != null)
+        {
+            go.transform.AddParent(parent);
+        }
+
+        return go;
+    }
 }
