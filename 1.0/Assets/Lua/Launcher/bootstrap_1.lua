9c203d26103f2645385a5062d9acd1f9038181e2
easygamesniper <cywu88@126.com>
2020-11-21 20:45:34 +0800

    更新启动流程

--- 1.0/Assets/Lua/Launcher/bootstrap_1.lua
+++ 1.0/Assets/Lua/Launcher/bootstrap_1.lua
@@ -3,15 +3,41 @@ print("bootstrap hero lua")
 local UnityEngine = CS.UnityEngine
 local GameObject = UnityEngine.GameObject
 
+local CSResMgr = CS.ResourceMgr_New.Instance
 
 local log = require 'Common.Logger'
+local respath = require 'Common.respath'
 local util = require 'XLua.common.util'
 
 local m = {}
+local RESOLUTION = {1366, 768}
 
 local initUIRoot = function()
 	local uiroot = GameObject('UIRoot')
   GameObject.DontDestroyOnLoad(uiroot)
+
+  local uicamera = CSResMgr:CreateGameObjectFromResources(respath.builtin.uicamera)
+  uicamera.name = 'UICamera'
+  uicamera.transform:SetParent(uiroot.transform, false)
+
+  local canvas = uiroot:AddComponent(typeof(UnityEngine.Canvas))
+  canvas.renderMode = UnityEngine.RenderMode.ScreenSpaceCamera
+  canvas.worldCamera = uicamera:GetComponent(typeof(UnityEngine.Camera))
+  canvas.planeDistance = 1000
+
+  local scaler = uiroot:AddComponent(typeof(UnityEngine.UI.CanvasScaler))
+  scaler.uiScaleMode = UnityEngine.UI.CanvasScaler.ScaleMode.ScaleWithScreenSize
+  scaler.referenceResolution = UnityEngine.Vector2(RESOLUTION[1], RESOLUTION[2])
+  local scaleX = UnityEngine.Screen.width / RESOLUTION[1]
+  local scaleY = UnityEngine.Screen.height / RESOLUTION[2]
+  scaler.matchWidthOrHeight = scaleX > scaleY and 1 or 0
+
+  local goevent = GameObject('EventSystem')
+  goevent:AddComponent(typeof(UnityEngine.EventSystems.EventSystem))
+  goevent:AddComponent(typeof(UnityEngine.EventSystems.StandaloneInputModule))
+  GameObject.DontDestroyOnLoad(goevent)
+
+
 end
 
 local loadLogin = function()
