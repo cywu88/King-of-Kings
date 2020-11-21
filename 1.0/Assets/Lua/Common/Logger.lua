830d9b0da99230baf9170d539970b866d119cdd0
easygamesniper <cywu88@126.com>
2020-11-21 20:20:37 +0800

    增加简单的日志系统 启动流程

--- 1.0/Assets/Lua/Common/Logger.lua
+++ 1.0/Assets/Lua/Common/Logger.lua
@@ -14,3 +14,32 @@ local function LogError(msg)
 		CS.Logger.LogError(debug.traceback(msg, 2))
 	end
 end
+
+Logger.Log = Log
+Logger.LogError = LogError
+
+--[[
+	方便打印多个值，用 | 分割
+]]
+local Time = Time or CS.UnityEngine.Time
+local function dump(...)
+	local tbl = table.pack(...)
+	local out = {Time.frameCount}
+	for i = 1, tbl.n do
+		table.insert(out, tostring(tbl[i]))
+	end
+	return table.concat(out, ' | ')
+end
+
+function Logger.d(...)
+	CS.Logger.Log(debug.traceback(dump(...), 2))
+end
+function Logger.e(...)
+	CS.Logger.LogError(debug.traceback(dump(...), 2))
+end
+function Logger.color(color, ...)
+	local s = string.format('<color=%s>%s</color>', color, dump(...))
+	CS.Logger.Log(debug.traceback(s, 2))
+end
+
+return Logger
--- 1.0/Assets/Lua/Launcher/bootstrap_1.lua
+++ 1.0/Assets/Lua/Launcher/bootstrap_1.lua
@@ -1,5 +1,59 @@
--- function main()
--- 	print("hello")
--- end
+print("bootstrap hero lua")
+
+local UnityEngine = CS.UnityEngine
+local GameObject = UnityEngine.GameObject
+
+
+local log = require 'Common.Logger'
+local util = require 'XLua.common.util'
+
 local m = {}
+
+local initUIRoot = function()
+	local uiroot = GameObject('UIRoot')
+  GameObject.DontDestroyOnLoad(uiroot)
+end
+
+local loadLogin = function()
+	 log.d('loadLogin...')
+end
+
+local initUI = function()
+  log.d('initUI')
+  initUIRoot()
+  loadLogin()
+end
+
+local wrapstep = function(fn, desc)
+  return function(...)
+    local ok, err = pcall(fn, ...)
+    if not ok then
+      local msg = 'BUILTIN_TEXT.UNKNOWN_ERROR' .. ': ' .. desc
+      log.e(msg, err)
+      coroutine.yield()
+    end
+  end
+end
+
+local start = function()
+  log.d('------------------', 'bootstrap_1 start')
+
+  local steps = {
+    initUI,
+  }
+
+  for k, v in ipairs(steps) do
+    local fn = wrapstep(v, k)
+    fn()
+  end
+  log.d('------------------', 'bootstrap_1 done')
+
+  m._mono.NeedDisplose = true
+end
+
+m.start = util.coroutine_call(start)
+m.update = function()
+  -- uilogin.update()
+end
+
 return m
